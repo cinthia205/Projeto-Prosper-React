43be171fe49d5b8c0c8ca40ce63e069000abfe13
cinthia205 <netscaper99@gmail.com>
2023-10-29 20:59:16 -0300

    typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Navbar from '@/components/Navbar'
+import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
